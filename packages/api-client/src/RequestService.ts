144bc2f3fbc1826b5cb88ef362a1bdce92c08504
Florian Keller <github@floriankeller.de>
2019-03-20 17:43:35 +0100

    feat(api-client): Add injector for RequestService

--- packages/api-client/src/RequestService.ts
+++ packages/api-client/src/RequestService.ts
@@ -3,6 +3,8 @@ import * as HTTP_STATUS from 'http-status-codes';
 
 import {ExceptionMapper, InvalidResponseError} from './APIException';
 
+export type InjectorFn = (baseConfig: AxiosRequestConfig) => AxiosRequestConfig;
+
 enum HttpMethod {
   DELETE = 'delete',
   GET = 'get',
@@ -16,71 +18,106 @@ enum HttpMethod {
 export class RequestService {
   constructor(private apiUrl: string) {}
 
-  public delete<T>(url: string, additionalOptions?: AxiosRequestConfig): Promise<T> {
-    const config = {
-      method: HttpMethod.DELETE,
-      url: this.apiUrl + url,
-      ...(!!additionalOptions && additionalOptions),
-    };
+  public delete<T>(url: string, optionsOrInjector?: AxiosRequestConfig | InjectorFn): Promise<T> {
+    const config = this.injectConfig(
+      {
+        method: HttpMethod.DELETE,
+        url: this.apiUrl + url,
+      },
+      optionsOrInjector
+    );
+
     return this.request<T>(config);
   }
 
-  public get<T>(url: string, additionalOptions?: AxiosRequestConfig): Promise<T> {
-    const config = {
-      method: HttpMethod.GET,
-      url: this.apiUrl + url,
-      ...(!!additionalOptions && additionalOptions),
-    };
+  public get<T>(url: string, optionsOrInjector?: AxiosRequestConfig): Promise<T> {
+    const config = this.injectConfig(
+      {
+        method: HttpMethod.GET,
+        url: this.apiUrl + url,
+      },
+      optionsOrInjector
+    );
+
     return this.request<T>(config);
   }
 
-  public head<T>(url: string, additionalOptions?: AxiosRequestConfig): Promise<T> {
-    const config = {
-      method: HttpMethod.HEAD,
-      url: this.apiUrl + url,
-      ...(!!additionalOptions && additionalOptions),
-    };
+  public head<T>(url: string, optionsOrInjector?: AxiosRequestConfig): Promise<T> {
+    const config = this.injectConfig(
+      {
+        method: HttpMethod.HEAD,
+        url: this.apiUrl + url,
+      },
+      optionsOrInjector
+    );
+
     return this.request<T>(config);
   }
 
-  public options<T>(url: string, additionalOptions?: AxiosRequestConfig): Promise<T> {
-    const config = {
-      method: HttpMethod.OPTIONS,
-      url: this.apiUrl + url,
-      ...(!!additionalOptions && additionalOptions),
-    };
+  public options<T>(url: string, optionsOrInjector?: AxiosRequestConfig): Promise<T> {
+    const config = this.injectConfig(
+      {
+        method: HttpMethod.OPTIONS,
+        url: this.apiUrl + url,
+      },
+      optionsOrInjector
+    );
+
     return this.request<T>(config);
   }
 
-  public patch<T>(url: string, additionalOptions?: AxiosRequestConfig): Promise<T> {
-    const config = {
-      method: HttpMethod.PATCH,
-      url: this.apiUrl + url,
-      ...(!!additionalOptions && additionalOptions),
-    };
+  public patch<T>(url: string, optionsOrInjector?: AxiosRequestConfig): Promise<T> {
+    const config = this.injectConfig(
+      {
+        method: HttpMethod.PATCH,
+        url: this.apiUrl + url,
+      },
+      optionsOrInjector
+    );
+
     return this.request<T>(config);
   }
 
-  public post<T>(url: string, additionalOptions?: AxiosRequestConfig): Promise<T> {
-    const config = {
-      method: HttpMethod.POST,
-      url: this.apiUrl + url,
-      ...(!!additionalOptions && additionalOptions),
-    };
+  public post<T>(url: string, optionsOrInjector?: AxiosRequestConfig): Promise<T> {
+    const config = this.injectConfig(
+      {
+        method: HttpMethod.POST,
+        url: this.apiUrl + url,
+      },
+      optionsOrInjector
+    );
+
     return this.request<T>(config);
   }
 
-  public put<T>(url: string, additionalOptions?: AxiosRequestConfig): Promise<T> {
-    const config = {
-      method: HttpMethod.PUT,
-      url: this.apiUrl + url,
-      ...(!!additionalOptions && additionalOptions),
-    };
+  public put<T>(url: string, optionsOrInjector?: AxiosRequestConfig): Promise<T> {
+    const config = this.injectConfig(
+      {
+        method: HttpMethod.PUT,
+        url: this.apiUrl + url,
+      },
+      optionsOrInjector
+    );
+
     return this.request<T>(config);
   }
 
   public setApiUrl(apiUrl: string): void {
     this.apiUrl = apiUrl;
+  }
+
+  private injectConfig(
+    baseConfig: AxiosRequestConfig,
+    optionsOrInjector?: AxiosRequestConfig | InjectorFn
+  ): AxiosRequestConfig {
+    if (typeof optionsOrInjector === 'function') {
+      return optionsOrInjector(baseConfig);
+    }
+
+    return {
+      ...baseConfig,
+      ...(!!optionsOrInjector && optionsOrInjector),
+    };
   }
 
   private async request<T>(config: AxiosRequestConfig): Promise<T> {
