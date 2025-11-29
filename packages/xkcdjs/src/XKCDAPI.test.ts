63d4dc72935bdccc31385a64f992baaf5db81892
Florian Imdahl <git@ffflorian.de>
2025-11-29 15:38:08 +0100

    update api-client

--- packages/xkcdjs/src/XKCDAPI.test.ts
+++ packages/xkcdjs/src/XKCDAPI.test.ts
@@ -80,7 +80,7 @@ describe('XKCD', () => {
     console.log('latestWithData.data:', latestWithData.data);
 
     expect(latestWithData.data).toMatchObject({
-      data: expect.any(Buffer),
+      data: expect.any(ArrayBuffer),
     });
   });
 
@@ -91,6 +91,7 @@ describe('XKCD', () => {
       await xkcdJS.api.getByIndex(1);
       expect.fail('Did not throw error');
     } catch (error) {
+      console.log((error as Error).message)
       expect((error as Error).message.includes('Request failed with status code 404')).toBe(true);
     }
   });
